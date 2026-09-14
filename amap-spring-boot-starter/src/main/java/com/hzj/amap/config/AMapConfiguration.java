59389de5924409f4f91df3e37b8efd9fbc2e423f
当夜晚来临 <2315912943@qq.com>
2026-09-14 11:43:20 +0800

    refactor：高德移除动态配置 Provider 兜底
    - 删除 AMapWebApiProperties 与 PropertiesAMapWebApiConfigProvider 实现
    - AMapConfiguration 条件由 @ConditionalOnProperty 改为 @ConditionalOnBean(AMapWebApiConfigProvider.class)

--- amap-spring-boot-starter/src/main/java/com/hzj/amap/config/AMapConfiguration.java
+++ amap-spring-boot-starter/src/main/java/com/hzj/amap/config/AMapConfiguration.java
@@ -2,34 +2,22 @@ package com.hzj.amap.config;
 
 import com.hzj.amap.core.webapi.AMapWebApiService;
 import com.hzj.amap.core.webapi.impl.DefaultAMapWebApiService;
-import com.hzj.amap.properties.AMapWebApiProperties;
 import com.hzj.amap.provider.webapi.AMapWebApiConfigProvider;
-import com.hzj.amap.provider.webapi.impl.PropertiesAMapWebApiConfigProvider;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 /**
  * 高德地图自动配置。
+ * <p>
+ * 仅当使用方提供了 {@link AMapWebApiConfigProvider} Bean 时才会装配。
  */
 @AutoConfiguration
-@EnableConfigurationProperties(AMapWebApiProperties.class)
+@ConditionalOnBean(AMapWebApiConfigProvider.class)
 public class AMapConfiguration {
-
-    /**
-     * 注册基于配置属性的高德 Web 服务 API 配置提供者。
-     *
-     * @param properties 高德 Web 服务 API 配置属性
-     * @return 高德 Web 服务 API 配置提供者
-     */
-    @Bean
-    @ConditionalOnMissingBean(AMapWebApiConfigProvider.class)
-    public AMapWebApiConfigProvider amapWebApiConfigProvider(AMapWebApiProperties properties) {
-        return new PropertiesAMapWebApiConfigProvider(properties);
-    }
 
     /**
      * 注册高德 Web 服务 API 服务。
